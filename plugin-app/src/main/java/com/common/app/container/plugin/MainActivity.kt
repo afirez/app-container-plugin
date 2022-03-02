6b71278b0f55f01451433246a4e4d39721a6b1f5
afirez <afirez.io@gmail.com>
2022-03-02 17:37:29 +0800

    first

---
+++ plugin-app/src/main/java/com/common/app/container/plugin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.common.app.container.plugin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tencent.shadow.sample.plugin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ plugin-app/src/main/java/com/tencent/shadow/sample/plugin/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tencent.shadow.sample.plugin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tencent.shadow.sample.plugin.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
